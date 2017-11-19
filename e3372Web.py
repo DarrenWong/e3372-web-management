6fff33c633d15b536d2d11c92ec343f2a9dc2ccb
Zbigniew Zasieczny <z.zasieczny@gmail.com>
2017-11-19 10:17:02 +0100

    added loger.propagate

--- e3372Web.py
+++ e3372Web.py
@@ -13,6 +13,7 @@ from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger('my_logger')
 logger.setLevel(logging.INFO)
+logger.propagate = True
 handler = RotatingFileHandler('logs/configlog.log', maxBytes=2000, backupCount=5)
 # create a logging format
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
